e282e23c091696cfb45f9f15ad80f8bff56212cf
dhlabanu <hlabanud1@gmail.com>
2025-08-29 10:19:20 +0200

    initial commit

---
+++ src/main/java/guru/springframework/sfgrestbrewery/web/model/CustomerDto.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgrestbrewery.web.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-04-21.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDto {
+
+    private UUID id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String name;
+}
